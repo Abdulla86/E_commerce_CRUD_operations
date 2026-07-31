f6676f49ec847653a8ba49735b332147747059ba
Abdulla86 <skabudulla346@gmail.com>
2026-07-31 15:30:36 +0530

    Added security to the sensitive information

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://root:Abdulla#12@localhost:3306/e_commerce"
+load_dotenv() # tells python to open .env file read the variables and place them in application env
+# DATABASE_URL = "mysql+pymysql://root:Abdulla#12@localhost:3306/e_commerce"
+DATABASE_URL = os.getenv("Db_URL")
 
 engine = create_engine(DATABASE_URL)
 
